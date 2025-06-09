54f2a4ad9d392e89489f79035d680e0a015cfbb4
Abbas0vv <ilhamabbasov46@gmail.com>
2025-06-10 03:03:06 +0400

    jobway.Web library added. Folders filled

---
+++ jobway.Core/Interfaces/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace jobway.Core.Interfaces;
+
+interface IEntity
+{
+    public string Id { get; set; }
+}
